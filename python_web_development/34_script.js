0f8f659be5061d0a7212c1066982150ecbf78e61
PulkitChahal <pulkitchahal07@gmail.com>
2024-07-14 18:02:43 +0530

    34_js

---
+++ python_web_development/34_script.js
@@ -0,0 +1,99 @@
+// -> Switch
+
+// Mini App based on user
+
+// let user = "student";
+// switch (user) {
+//   case "admin":
+// console.log("Welcome admin!");
+// break;
+//   case "teacher":
+// console.log("Welcome teacher!");
+// break;
+//   case "student":
+// console.log("Welcome student!");
+// break;
+//   default:
+// console.log("Unknown user role.");
+// }
+
+// -> Date
+
+// const date = new Date();
+// console.log(date);
+// console.log(date.toString());
+// console.log(date.getDay());
+// console.log(date.getFullYear());
+// console.log(date.getMonth());
+// console.log(date.getMinutes());
+// console.log(date.getHours());
+
+// -> Array
+
+// const arr1 = [];
+// console.log(arr1);
+
+// let names = ['Pulkit', 'Gupta', 'Sudhir', 'Suraj', 'Pankaj'];
+// console.log(names);
+// console.log(names[2]);
+
+// names.push('Aman');
+// console.log(names);
+// console.log(names.length);
+
+// console.log(names[names.length - 1]);
+// console.log(names[names.length - 2]);
+
+// console.log(names.slice(1, 4));
+
+// // names.splice(index, howmany, item);
+// names.splice(1, 3, 'item1', 'item2', 'item3', 'item4', 'item5');
+// console.log(names);
+
+// let lastname = ['abc', 'def', 'ghi', 'jkl', 'kumar'];
+// let middlename = [1, 2, 3, 4, 5]
+// let full_name = names.concat(lastname, middlename);
+// console.log(full_name);
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(numbers);
+// console.log(numbers.copyWithin(3, 2));
+// console.log(numbers.copyWithin(1, 2, 4));
+// console.log(numbers.copyWithin(2, 3, 6));
+
+// let names = ["Anurag", "Gupta", "Sudhir", "Suraj", "HiteshSir", "Ankur"];
+// console.log(names.includes("HiteshSir"));
+// console.log(names.includes("Ankur", 4));
+// console.log(names.includes("Pulkit", 4));
+// console.log(names.indexOf("Sudhir"));
+// console.log(Array.isArray(names));
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 8, 545, 45, 4, 6, 45, 35, 29, 27];
+// console.log(numbers.lastIndexOf(45));
+
+// let num = [1, 2, 3, 4, 5];
+// console.log(num.map(Math.sqrt));
+// let num1 = [1, 4, 9, 16, 25];
+// console.log(num1.map(Math.sqrt));
+
+// let num = [1, 4, 9, 16, 25];
+// console.log(num.pop());
+// console.log(num.shift());
+// console.log(num);
+// console.log(num.reverse());
+
+// let num = [1, 35, 98, 16, 25];
+// console.log(num);
+// console.log(num.sort());
+// num.unshift(91, 71);
+// console.log(num);
+// num.unshift(72, 92);
+// console.log(num);
+
+// let names = "javascript";
+// let namess = names.split("");
+// console.log(namess);
+
+// let company = 'ineuron, facebook, amazon, google';
+// let companies = company.split(', ');
+// console.log(companies);
